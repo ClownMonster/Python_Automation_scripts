fb246a1934a7118fbe4c2618707a9087fec7c87a
ClownMonster <mk6386223@gmail.com>
2020-05-18 20:44:21 +0530

    twitter folower data

---
+++ twitterFollowers.py
@@ -0,0 +1,15 @@
+'''
+This scripts gets the followers of the required twitter account
+
+'''
+
+import tweepy
+
+
+auth  = tweepy.OAuthHandler(consumer_key = "REMOVED", consumer_secret = "REMOVED")
+auth.set_access_token(key = "REMOVED", secret = "REMOVED") 
+
+api = tweepy.API(auth)
+user  = tweepy.api.get_user()
+#print(user.screen_name)
+
